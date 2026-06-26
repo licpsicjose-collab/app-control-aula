d39c3c2ba370f6e16e7533150c278df4429244dd
licpsicjose-collab <licpsicjose@gmail.com>
2026-06-26 14:15:44 -0600

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,17 +1,10 @@
-const firebaseConfig = {const apiKey: "AIzaSyDRjBwW6nueplRNYL-S0sxfliC8taWNZCM",
+const firebaseConfig = {
+  apiKey: "AIzaSyDRjBwW6nueplRNYL-S0sxfliC8taWNZCM",
   authDomain: "app-clase-3a564.firebaseapp.com",
   projectId: "app-clase-3a564",
-  storageBucket: "app-clase-3a564.appspot.com",
-  messagingSenderId: "206818130286",
-  appId: "1:206818130286:web:d04f2ae584dfc3cb4e297f"
 };
 
-// ✅ INICIAR FIREBASE
 firebase.initializeApp(firebaseConfig);
 
-// ✅ FIRESTORE
-const db = firebase.firestore();
-
-// ✅ 🔐 AUTH (ESTO ES LO QUE TE FALTABA)
+// ✅ ESTE ES EL MÁS IMPORTANTE
 const auth = firebase.auth();
-

b40e311f729da40274def6b4d1c94862a9057ec8
licpsicjose-collab <licpsicjose@gmail.com>
2026-06-26 17:31:10 -0600

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -2,9 +2,15 @@ const firebaseConfig = {
   apiKey: "AIzaSyDRjBwW6nueplRNYL-S0sxfliC8taWNZCM",
   authDomain: "app-clase-3a564.firebaseapp.com",
   projectId: "app-clase-3a564",
+  storageBucket: "app-clase-3a564.appspot.com",
+  messagingSenderId: "206818130286",
+  appId: "1:206818130286:web:d04f2ae584dfc3cb4e297f"
 };
 
 firebase.initializeApp(firebaseConfig);
 
-// ✅ ESTE ES EL MÁS IMPORTANTE
+// Base de datos
+const db = firebase.firestore();
+
+// Autenticación
 const auth = firebase.auth();

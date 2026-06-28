361f97058291838335c9d380dd88f99ba1e47d91
licpsicjose-collab <licpsicjose@gmail.com>
2026-06-28 01:07:39 -0600

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// ✅ CONFIGURACIÓN DE FIREBASE
+const firebaseConfig = {
+  apiKey: "AIzaSyDRjBwW6nueplRNYL-S0sxfliC8taWNZCM",
+  authDomain: "app-clase-3a564.firebaseapp.com",
+  projectId: "app-clase-3a564",
+  storageBucket: "app-clase-3a564.appspot.com",
+  messagingSenderId: "206818130286",
+  appId: "1:206818130286:web:d04f2ae584dfc3cb4e297f"
+};
+
+// ✅ INICIALIZAR FIREBASE (UNA SOLA VEZ EN TODA LA APP)
+firebase.initializeApp(firebaseConfig);
+
+// ✅ BASE DE DATOS (LO QUE USA TODO EL SISTEMA)
+const db = firebase.firestore();
